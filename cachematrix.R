5e2c3fdc9bfcd6fd4e34ca29e863b7a2b305e347
CTDataScience <anunags@gmail.com>
2017-07-16 21:12:40 -0400

    Final cacheInverse assignment

--- cachematrix.R
+++ cachematrix.R
@@ -27,7 +27,7 @@ cacheSolve <- function(x, ...) {
   m <- x$getinverse()
   if (!is.null(m)) {
     message("getting cached data")
-    return(i)
+    return(m)
   }
   data <- x$get()
   m <- solve(data, ...)
